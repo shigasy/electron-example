41b4beddcdff63dd2f7fbb4b436de3b9e3fd49a8
shigasy <kxx555u.u@gmail.com>
2020-06-22 22:55:54 +0900

    add: ぐちゃぐちゃやけど、他アプリ上でもグローバルに呼び出せるようになったのでcommit。たぶんMacしか動かない

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { app, BrowserWindow, globalShortcut } from 'electron';
 import path from 'path';
+import url from 'url';
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 import hotReload from 'electron-reloader';
 
@@ -32,12 +34,103 @@ const createWindow = () => {
   return win;
 };
 
+let isOpen = true;
+
+const closeMainWindow = () => {
+  isOpen = false;
+};
+
+const createMainWindow = () => {
+  const main = new BrowserWindow({
+    width: 600,
+    height: 500,
+    frame: false, // 枠の無いウィンドウ
+    resizable: false, // ウィンドウのリサイズを禁止
+    show: false,
+    alwaysOnTop: true,
+    hasShadow: false,
+    webPreferences: {
+      // レンダラープロセスで Node.js 使えないようにする (XSS対策)
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false,
+      // preload で実行するときに、コンテキスト(this == window)を別なものとする
+      contextIsolation: true,
+      // process や Electron を windowオブジェクト に保存する処理。フルパスの指定が必要
+      preload: path.join(__dirname, './core/preLoad.js'), // preLoad.js にビジネスロジックを記述する
+    },
+  });
+  main.setVisibleOnAllWorkspaces(true);
+
+  main.setAlwaysOnTop(true, 'screen-saver', 1);
+  main.on('closed', closeMainWindow);
+
+  // main.setVisibleOnAllWorkspaces(true);
+  main.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true,
+      hash: 'main',
+    })
+  );
+  return main;
+};
+
+// --------------
+
+const openMainWindow = () => {
+  closeMainWindow();
+  createMainWindow();
+  isOpen = true;
+};
+
+const isMainWindowOpen = () => isOpen;
+
 // Electronの起動準備が終わったら、ウィンドウを作成する。
 app.whenReady().then(() => {
-  const win = createWindow();
+  const main = createMainWindow();
+
+  if (!main.isVisible()) {
+    app.dock.hide();
+    main.showInactive();
+
+    // And also hide it after a while
+    setTimeout(() => {
+      main.hide();
+      app.dock.show();
+    }, 1000);
+  }
+
+  // const win = createWindow();
+  // app.dock.hide();
+
+  // const main = createMainWindow();
   const ret = globalShortcut.register('CommandOrControl+X', () => {
     console.log('CommandOrControl+X is pressed');
-    win.show();
+    if (isOpen) {
+      main.hide();
+      app.dock.show();
+    } else {
+      app.dock.hide();
+      main.showInactive();
+    }
+    isOpen = !isOpen;
+
+    // if (isMainWindowOpen()) {
+    //   closeMainWindow();
+    //   // hides the dock icon for our app which allows our windows to join other
+    //   // apps' spaces. without this our windows open on the nearest "desktop" space
+    //   // main.setAlwaysOnTop(true, 'floating');
+    //   // main.setVisibleOnAllWorkspaces(true);
+    //   // main.setFullScreenable(false);
+
+    //   // app.dock.show();
+    //   console.log('close');
+    // } else {
+    //   app.dock.show();
+
+    //   openMainWindow();
+    // }
   });
 
   if (!ret) {
@@ -47,6 +140,8 @@ app.whenReady().then(() => {
   // Check whether a shortcut is registered.
   console.log(globalShortcut.isRegistered('CommandOrControl+X'));
 });
+
+app.on('before-quit', closeMainWindow);
 
 // すべての ウィンドウ が閉じたときの処理
 app.on('window-all-closed', () => {
