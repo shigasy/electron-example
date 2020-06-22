f84a12100a2b09d45bbc6e0e76d28f0a9870b8ff
shigasy <kxx555u.u@gmail.com>
2020-06-22 12:27:50 +0900

    add: 動的な保存機能

--- src/components/Save.tsx
+++ src/components/Save.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import '../types/core';
+import React, { useState, useCallback } from 'react';
+// import '../types/core';
 
 const Save: React.FC = () => {
+  const [body, setBody] = useState('');
   const onSaveClick = () => {
-    window.core.saveText('test');
+    window.core.saveText(body);
     console.log('save');
   };
+  const onLoadClick = () => {
+    // window.core.saveText('test');
+    console.log('load');
+  };
+
+  const onBodyChange = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const { value } = e.currentTarget;
+      setBody(value);
+    },
+    [body]
+  );
   return (
     <div>
+      <textarea onChange={onBodyChange} />
       <button type="button" onClick={onSaveClick}>
         Save
+      </button>
+      <button type="button" onClick={onLoadClick}>
+        Load
       </button>
     </div>
   );
